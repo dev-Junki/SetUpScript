14795731e8d8f28f91da24ea4a391bbaa4525acb
Junki mac <infoxjunki@icloud.com>
2021-01-01 20:39:16 +0900

    add node command

--- node.sh
+++ node.sh
@@ -2,3 +2,20 @@
 NAME=Junki
 echo "Hello, $NAME!"
 echo -e "Let's starting setup!\n"
+
+#install Homebrew
+#/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+#install nodebrew
+#brew install
+
+#nodebrew setup initalize
+#/usr/local/opt/nodebrew/bin/nodebrew setup_dirs
+
+#export path
+#echo 'export PATH=$HOME/.nodebrew/current/bin:$PATH' >> ~/.bashrc
+#echo 'export NODEBREW_ROOT=/usr/local/var/nodebrew' >> ~/.bshrc
+
+
+#confirm node version
+node -v
